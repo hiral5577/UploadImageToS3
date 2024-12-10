e9fc9d52da9c45e8dae878ecdfe834af20ba96e5
hiral5577 <153362254+hiral5577@users.noreply.github.com>
2024-12-10 07:44:28 -0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,7 +33,7 @@ export default {
     data () {
       return {
         image: '',
-        uploadURL: 'ec2-54-226-75-67.compute-1.amazonaws.com:8080'
+        uploadURL: ''
       }
     },
   methods: {
