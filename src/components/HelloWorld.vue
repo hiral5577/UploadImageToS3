d9401daa1c820700c34677b33dc95a98a975e8d7
hiral5577 <153362254+hiral5577@users.noreply.github.com>
2025-10-09 19:23:01 -0700

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://wy1lr7076d.execute-api.us-east-1.amazonaws.com/default/s3image'
+const API_ENDPOINT = 'https://ibzk4irkki.execute-api.us-east-1.amazonaws.com/default/demo'
 
 export default {
   name: 'app',
